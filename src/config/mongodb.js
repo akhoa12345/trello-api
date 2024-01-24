aaf9457e859200bd9a6298791bc032a8c254305e
Anh Khoa <phunganhkhoa123@gmail.com>
2024-01-24 21:56:56 +0700

    47. Tối ưu kết nối MongoDB Atlas vào dự án Back-end

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,3 +1,37 @@
-/**
- *
- */
+const MONGODB_URI = 'mongodb+srv://phunganhkhoa123:WwJGJ8GLxa0aNthe@cluster0-anhkhoa.ncbndiw.mongodb.net/?retryWrites=true&w=majority'
+
+const DATABASE_NAME = 'trello'
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+// Khởi tạo 1 đối tượng trelloDatabaseInstance ban đầu là null (vì chúng ta chưa connect)
+let trelloDatabaseInstance = null
+
+// Khởi tạo 1 đối tượng mongoClientInstance để connect tới MongoDB
+const mongoClientInstance = new MongoClient(MONGODB_URI, {
+  // Lưu ý: cái serverApi có từ phiên bản 5.0.0 trở lên, có thể không cần dùng nó, còn nếu dùng
+  // nó là chúng ta sẽ chỉ định 1 cái Stable API Version của MongoDB
+  // Đọc thêm ở đây: https://www.mongodb.com/docs/drivers/node/current/fundamentals/stable-api/
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+// Kết nối tới Database
+export const CONNECT_DB = async () => {
+  // Gọi kết nối tới MongoDB Atlas với URI đã khai báo trong thân của mongoClientInstance
+  await mongoClientInstance.connect()
+
+  // Kết nối thành công thì lấy ra Database theo tên và gán ngược nó lại vào biến trelloDatabaseInstance ở trên của chúng ta
+  trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME)
+}
+
+// Function GET_DB (không async) này có nhiệm vụ export ra cái Trello Database Instance sau khi đã connect
+// thành công tới MongoDB để chúng ta sử dụng ở nhiều nơi khác nhau trong code.
+// Lưu ý phải đảm bảo chỉ luôn gọi cái  GET_DB này sau khi đã kết nối thành công tới MongoDB
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error('Must connect to Database first!')
+  return trelloDatabaseInstance
+}
--- src/server.js
+++ src/server.js
@@ -1,27 +1,46 @@
+/* eslint-disable no-console */
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
 
-const app = express()
+import { CONNECT_DB, GET_DB } from '~/config/mongodb'
 
-const hostname = 'localhost'
-const port = 8017
+const START_SERVER = () => {
+  const app = express()
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  // eslint-disable-next-line no-console
-  console.log(mapOrder(
-    [{ id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' }],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  const hostname = 'localhost'
+  const port = 8017
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello, I am running at http://${ hostname }:${ port }/`)
-})
+  app.get('/', async(req, res) => {
+    console.log(await GET_DB().listCollections().toArray())
+
+    res.end('<h1>Hello World!</h1><hr>')
+  })
+
+  app.listen(port, hostname, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Hello, I am running at http://${ hostname }:${ port }/`)
+  })
+}
+
+// Chỉ khi kết nối tới Database thành công thì mới Start Server Back-end lên
+// Immediately Invoked / Anonymous Async Function Expression (IIFE)
+(async () => {
+  try {
+    await CONNECT_DB()
+    console.log('Connected to MongoDB Atlas Cloud success!')
+
+    // Khởi động server Back-end sau khi Connect Database thành công
+    START_SERVER()
+  } catch (error) {
+    console.error(error)
+    process.exit(0)
+  }
+})()
+
+// Chỉ khi kết nối tới Database thành công thì mới Start Server Back-end lên
+// CONNECT_DB()
+//   .then(() => console.log('Connected to MongoDB Atlas Cloud success!'))
+//   .then(() => START_SERVER())
+//   .catch(error => {
+//     console.error(error)
+//     process.exit(0)
+//   })
