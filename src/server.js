e31d0f323957a7fe482d1e35f06ddad0091debab
Anh Khoa <phunganhkhoa123@gmail.com>
2024-01-27 10:02:28 +0700

    50. Viết API với Express Router, hiểu HTTP Status Codes

--- src/server.js
+++ src/server.js
@@ -4,13 +4,12 @@ import exitHook from 'async-exit-hook'
 
 import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
 import { env } from '~/config/environment'
+import { APIs_V1 } from '~/routes/v1'
 
 const START_SERVER = () => {
   const app = express()
 
-  app.get('/', async(req, res) => {
-    res.end('<h1>Hello World!</h1><hr>')
-  })
+  app.use('/v1', APIs_V1)
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     // eslint-disable-next-line no-console
