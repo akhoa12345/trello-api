286e72f7403f04f550e0660f6b960cc124b2d739
Anh Khoa <phunganhkhoa123@gmail.com>
2024-01-30 13:15:06 +0700

    53. Code tầng Controller: Điều hướng dữ liệu

--- src/routes/v1/boardRoute.js
+++ src/routes/v1/boardRoute.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { StatusCodes } from 'http-status-codes'
 
+import { boardController } from '~/controllers/boardController'
 import { boardValidation } from '~/validations/boardValidation'
 
 const Router = express.Router()
@@ -9,6 +10,6 @@ Router.route('/')
   .get((req, res) => {
     res.status(StatusCodes.OK).json({ message: 'GET: API get list boards ' })
   })
-  .post(boardValidation.createNew)
+  .post(boardValidation.createNew, boardController.createNew)
 
 export const boardRoute = Router
