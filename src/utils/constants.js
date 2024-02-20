61af5f6b2387753a29dc31dbf1e737e411c1b6b3
Anh Khoa <phunganhkhoa123@gmail.com>
2024-02-20 20:25:24 +0700

    Update Whitelist Domains

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 // Những domain được phép truy cập tới tài nguyên của Server
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173' // Không cần localhost nữa vì ở file config/cors.js đã luôn luôn cho phép môi trường dev (env.BUILD_MODE === 'dev')
+  'https://trello-web-liard.vercel.app'
   // ...vv ví dụ sau này sẽ deploy lên domain chính thức ...vv
 ]
 
